b1a5bd05aaeb125a661b7098e96a2858e381e50c
chandrasekhar anabatula <acs3985@gmail.com>
2024-05-17 14:13:55 +0530

    Luasnip input node  feature added

--- lua/acs3985/users/snipps.lua
+++ lua/acs3985/users/snipps.lua
@@ -1,5 +1,6 @@
 local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
@@ -85,7 +86,9 @@ ls.add_snippets("lua", {
 })
 
 -----------end examples-----------
-ls.add_snippets("lua", { -- filetype declaration
+-----------new examples for further learning----------
+-- ------------autotrigger---------------
+ls.add_snippets("tex", { -- filetype declaration
 	-- Example How to set snippet parameters.
 	s(
 		{ -- Table 1: snippet parameters
@@ -99,5 +102,80 @@ ls.add_snippets("lua", { -- filetype declaration
 			t("Hello, World"),
 		}
 		-- Table 3: The advanced snippet options, left blank.
+	),
+})
+
+-----Snippets for Latex-----
+
+---Snippets for letters---------
+-- Note that you have to escape the backslash character
+-- to insert it literally—for example I have to write t("\\alpha")
+-- to produce the string \alpha in the first snippet.
+ls.add_snippets("tex", {
+	s({ trig = ";a", snippetType = "autosnippet" }, {
+		t("\\alpha"),
+	}),
+	s({ trig = ";b", snippetType = "autosnippet" }, {
+		t("\\beta"),
+	}),
+	s({ trig = ";g", snippetType = "autosnippet" }, {
+		t("\\gamma"),
+	}),
+})
+
+--- Combination for insert and textnodes for few boiler plates ---
+
+ls.add_snippets("tex", {
+	s({ trig = "tt", dscr = "Expands 'tt' into '\texttt{}' " }, {
+		t("\\texttt{"), -- remember : backslash need to be escaped.
+		i(1, "text"), -- 1 for dynamic node value, text is default value.
+		t("}"),
+	}),
+	-- Yes, these jumbles of text nodes and insert nodes get messy fast, and yes,
+	-- there is a much better, human-readable solution: ls.fmt, described shortly.
+	s({ trig = "ff", dscr = "Expands 'ff' into '\frac{}{}' " }, {
+		t("\\frac{"), -- remember : backslash need to be escaped.
+		i(1, "x"), -- 1 for dynamic node value, text is default value.
+		t("}"),
+		t("{"),
+		i(2, "y"),
+		t("}"),
+		i(0), -- final position of curser after done with snippets.
+	}),
+})
+
+-- Format: a human-friendly syntax for writing snippets
+
+ls.add_snippets("tex", {
+	-- The same equation snippet, using LuaSnip's fmt function.
+	-- The snippet is not shorter, but it is more *human-readable*.
+	s(
+		{ trig = "eq", dscr = "A LaTeX equation environment" },
+		fmt( -- The snippet code actually looks like the equation environment it produces.
+			[[
+      \begin{equation}
+          <>
+      \end{equation}
+    ]],
+			-- The insert node is placed in the <> angle brackets
+			{ i(1, "a + b = c") },
+			-- This is where I specify that angle brackets are used as node positions.
+			{ delimiters = "<>" }
+		)
+	),
+})
+
+--- using fmta table---
+ls.add_snippets("tex", {
+	s(
+		{ trig = "cen", dscr = "A latex Center Environment" },
+		fmta(
+			[[
+       \begin{center}
+           <>
+       \end{center}
+     ]],
+			{ i(1, "Your Text") }
+		)
 	),
 })
