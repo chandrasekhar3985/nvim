360903ef7fd2b57b3ffc77b9586512d1cef44867
chandrasekhar anabatula <acs3985@gmail.com>
2024-05-19 01:08:45 +0530

    snippet exp in progress, with sucess

--- lua/acs3985/users/snipps.lua
+++ lua/acs3985/users/snipps.lua
@@ -67,11 +67,15 @@ ls.add_snippets("all", {
 		t("Chandrasekhar Anabatula"),
 	}),
 })
-
+----------Snippets for latex-------------
+---------Snippets for line begin---------
 ls.add_snippets("tex", {
 	s(
 		{ trig = "cla", dscr = "documentclass", snippetType = "autosnippet" },
-		fmta([[ \documentclass[<>]{<>} ]], { i(1, "12pt, a4paper"), i(2, "artile") }),
+		fmta(
+			[[ \documentclass[<>]{<>} % article, book, report, Thesis, Journal ]],
+			{ i(1, "12pt, a4paper"), i(2, "artile") }
+		),
 		{ condition = line_begin }
 	),
 	s(
@@ -82,20 +86,100 @@ ls.add_snippets("tex", {
 		}),
 		{ condition = line_begin }
 	),
+	s(
+		{ trig = "bg", dscr = "A generic new environmennt", snippetType = "autosnippet" },
+		fmta(
+			[[
+     \begin{<>}
+         <>
+     \end{<>}
+   ]],
+			{
+				i(1),
+				i(2),
+				rep(1),
+			}
+		),
+		{ condition = line_begin }
+	),
+	s(
+		{ trig = "h1", dscr = "part", snippetType = "autosnippet" },
+		fmta([[\part{<>}]], { i(1, "book part name") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "h2", dscr = "chapter", snippetType = "autosnippet" },
+		fmta([[\chapter{<>}]], { i(1, "Book Chapter name") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "h3", dscr = "section", snippetType = "autosnippet" },
+		fmta([[\section{<>}]], { i(1, "section") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "h4", dscr = "subsection", snippetType = "autosnippet" },
+		fmta([[\subsection{<>}]], { i(1, "subsection") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "h4", dscr = "subsubsection", snippetType = "autosnippet" },
+		fmta([[\subsubsection{<>}]], { i(1, "subsubsection") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "h5", dscr = "paragraph", snippetType = "autosnippet" },
+		fmta([[\paragraph{<>}]], { i(1, "paragraph") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+
+		{ trig = "h6", dscr = "subparagraph", snippetType = "autosnippet" },
+		fmta([[\subparagraph{<>}]], { i(1, "subparagraph") }),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
+	s(
+		{ trig = "mv", dscr = "math visual mode", snippetType = "autosnippet" },
+		fmta(
+			[[ 
+            \[
+            <>
+            \]
+         ]],
+			{ i(1, "x + y") }
+		),
+		{ condition = line_begin } -- set condition in the `opts` table
+	),
 })
 
-ls.add_snippets("lua", { -- use of choice node
+ls.add_snippets("tex", {
 	s(
-		"k",
-		c(1, {
-			fmt("local {} = {}", {
-				i(1, "value"),
-				i(2, "text"),
-			}),
-			fmt("{} = {}", {
-				i(1, "value"),
-				i(2, "text"),
-			}),
+		{ trig = "([^%a])mm", wordTrig = false, regTrig = true },
+		fmta("<>$<>$", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			d(1, get_visual),
 		})
+	),
+	s(
+		{ trig = "([^%a])ee", regTrig = true, wordTrig = false },
+		fmta("<>e^{<>}", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			d(1, get_visual),
+		})
+	),
+})
+
+ls.add_snippets("tex", {
+	s(
+		{ trig = "l11", dscr = "HUGE", snippetType = "autosnippet" },
+		fmta([[<> {\HUGE <>  }]], {
+			i(1),
+			d(1, get_visual),
+		}),
+		{ condition = line_begin }
 	),
 })
