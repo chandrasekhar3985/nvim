40ef17bd35beca72931c3b3b607f475e254dff49
chandrasekhar anabatula <acs3985@gmail.com>
2024-06-05 21:51:09 +0530

    Multi-cursor plugin added

--- a.md
+++ a.md
@@ -2,9 +2,9 @@
 <html>
   <body>
 
-    <h1>My First Heading</h1>
-
-    <p>My first paragraph.</p>
-
-  </body>
-</html>
+    <h1>My First Hading</h1>This is Multi Cursor added.  
+ right awqaeklk 
+    <right awqaep>My firs paragraph.</p>  
+  
+  </body>This is Multi Cursor added.   
+</html>This is Multi Cursor addedright awqae.       hg
--- lua/acs3985/plugins/which-key.lua
+++ lua/acs3985/plugins/which-key.lua
@@ -1,3 +1,11 @@
+local function visual_cursors_with_delay()
+	-- Execute the vm-visual-cursors command.
+	vim.cmd('silent! execute "normal! \\<Plug>(VM-Visual-Cursors)"')
+	-- Introduce delay via VimScript's 'sleep' (set to 500 milliseconds here).
+	vim.cmd("sleep 200m")
+	-- Press 'A' in normal mode after the delay.
+	vim.cmd('silent! execute "normal! A"')
+end
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
@@ -264,6 +272,14 @@ return {
 				s = { "<Plug>(nvim-surround-normal)", "surround" },
 				d = { "<Plug>(nvim-surround-delete)", "delete" },
 				c = { "<Plug>(nvim-surround-change)", "change" },
+			},
+			z = {
+				name = "Visual Multi",
+				a = { "<Plug>(VM-Select-All)<Tab>", "Select All", mode = { "n" } },
+				r = { "<Plug>(VM-Start-Regex-Search)", "Start Regex Search", mode = { "n" } },
+				p = { "<Plug>(VM-Add-Cursor-At-Pos)", "Add Cursor At Pos", mode = { "n" } },
+				v = { visual_cursors_with_delay, "Visual Cursors", mode = { "v" } },
+				o = { "<Plug>(VM-Toggle-Mappings)", "Toggle Mapping", mode = { "n" } },
 			},
 			t = {
 				name = "TEMPLATES",
